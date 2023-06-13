8dd6267ea18915d5b524de082dd5fa6ba51aa2a6
Freemasoid <freemasoid@gmail.com>
2023-06-13 22:22:35 +0200

    return to the top button added

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ function reveal() {
     element.style.opacity = 1;
   }
 }
+
+let mybutton = document.getElementById("topBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
